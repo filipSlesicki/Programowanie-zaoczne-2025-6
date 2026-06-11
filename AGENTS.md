2b8978429cdb14021e7b333f61c2b487f6736ca4
Kamisia-git <kamila.radosz@op.pl>
2026-06-11 20:37:56 +0200

    New Bird Added

---
+++ AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Programowanie-zaoczne-2025-6
+- Unity version: Unity 6000.0.58f1
+- Active game object:
+  - Name: SpeedUpBird Variant
+  - Tag: Bird
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Birds/Scripts/Bird.cs
+++ Assets/Birds/Scripts/Bird.cs
@@ -1,17 +1,57 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
     public Rigidbody rigidbody;
     public float activationSpeed = 10;
+    [SerializeField] Bird birdPrefab;
+
+    float timeToSplit = .7f;
+    float timeElapsed = 0f;
+    bool wasSplit = false;
+    bool wasActivated = false;
 
     private void Update()
     {
-        
+        // Debug.Log("Update");
+        if (!wasActivated) return;
+        if (wasSplit) return;
+
+        timeElapsed += Time.deltaTime; // Timer
+
+        if (timeElapsed >= timeToSplit)
+        {
+            Spilt();
+
+        }
     }
 
     public virtual void Activate()
     {
-        
+
+    }
+
+
+    public void Shoot()
+    {
+        // Debug.Log("Shoot!");
+        wasActivated = true;
+    }
+
+    private void Spilt()
+    {
+        var b1 = Instantiate(birdPrefab, transform.position, Quaternion.identity);
+        var b2 = Instantiate(birdPrefab, transform.position, Quaternion.identity);
+        var b3 = Instantiate(birdPrefab, transform.position, Quaternion.identity);
+
+        b1.rigidbody.linearVelocity = rigidbody.linearVelocity;
+        b2.rigidbody.linearVelocity = rigidbody.linearVelocity;
+        b3.rigidbody.linearVelocity = rigidbody.linearVelocity;
+
+        timeElapsed = 0f;
+        wasSplit = true;
+
+        // TODO Destroy this go
     }
 }
